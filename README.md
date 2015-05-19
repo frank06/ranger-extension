fa3cbd19ab8317ff0e6f17b292aa15a6a7963bba
frank06 <me@frank06.net>
2015-05-19 12:24:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Ranger extension
+
+Highlight any web page with the [Ranger](https://github.com/frank06/ranger) library.
+
+Works on Chrome.
+
+## Usage
+
+Install extension.
+
+Select text. Hit 'H' key to highlight.
+
+To remove a highlight, click on it and hit 'H' again.
+
+Highlights will be kept in local storage.
+
+Soon these highlights will be synced with a web service running [ranger-api](https://github.com/frank06/ranger-api).
+
+## License
+
+Copyright ©2015 github.com/frank06
+
+Licensed under the MIT license.
---
+++ extension.js
@@ -0,0 +1,87 @@
+/* Issues
+
+  - Not saving/retrieving annotations: http://blog.arkofwellness.com/progest-e-my-favorite-product/
+
+*/
+
+var hl = function() {
+  var wrapper = document.createElement('span');
+  wrapper.className = "ranger-hl";
+  return wrapper;
+}();
+
+var style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = '.ranger-hl { background: rgba(255,255,10,0.5); cursor: default }';
+document.getElementsByTagName('head')[0].appendChild(style);
+
+// chrome.storage.local.remove(window.location.href);
+
+setTimeout(function() {
+
+  chrome.storage.local.get(window.location.href, function(data) {
+    var storage = data[window.location.href];
+    if (!storage) return;
+
+    Object.keys(storage).forEach(function(key) {
+      var range = storage[key];
+      console.log('Attempting to paint', key);
+      console.log(range);
+      new Ranger(range).paint(hl);
+    });
+
+  });
+  
+}, 4000);
+
+
+
+document.onkeyup = function(e) {
+  
+  if (e.which == 72) {
+    
+    if (window.getSelection() && window.getSelection().rangeCount > 0) {
+      range = window.getSelection().getRangeAt(0);
+      
+      if (range.collapsed) {
+        var id = Ranger.utils.unpaintBySampleNode(range.startContainer);
+        removeRange(id);
+        return;
+      }
+      
+      var ranger = new Ranger(range);
+      
+      addRange(ranger.toJSON());
+
+      ranger.paint(hl);
+      window.getSelection().removeAllRanges();
+    }
+    
+  }
+  
+};
+
+var addRange = function(range) {
+  
+  chrome.storage.local.get(window.location.href, function(storage) {
+    
+    storage[window.location.href] = storage[window.location.href] || {};
+    
+    storage[window.location.href][range.id] = range;
+    chrome.storage.local.set(storage);
+    
+  });
+  
+};
+
+var removeRange = function(id) {
+  
+  chrome.storage.local.get(window.location.href, function(storage) {
+    
+    delete storage[window.location.href][id]; 
+    
+    chrome.storage.local.set(storage);
+    
+  });
+  
+};
